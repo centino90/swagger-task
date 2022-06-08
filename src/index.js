cab8ed65cf4d403f6a438538e1a83541a0c239f9
Anthony jay Ansit <62681826+centino90@users.noreply.github.com>
2022-06-08 09:24:55 +0800

    redone index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import koa from 'koa';
 import swagger2Koa from 'swagger2-koa';
 import * as swagger from 'swagger2';
 import Router from '@koa/router';
-import bodyParser from 'koa-bodyparser';
+
 import { routes as registerRoute } from './routes/register.js';
 import { routes as loginRoute } from './routes/login.js';
 import { routes as createPostRoute } from './routes/createPost.js';
@@ -19,19 +19,16 @@ if (!swagger.validateDocument(spec)) {
 const port = process.env.PORT || 3000;
 
 const app = new koa();
-const router = Router({prefix: spec.basePath});
+const router = Router();
 
 for (const route of [registerRoute, loginRoute, createPostRoute, postsRoute]) {
   route(router);
 }
 
-app.use(bodyParser());
-
-app.use(validate(spec));
-
 app.use(router.routes());
 app.use(router.allowedMethods());
 
+app.use(validate(spec));
 app.use(ui(spec, '/docs'));
 
 app.listen(port, console.log(`Server on ${port}`));
